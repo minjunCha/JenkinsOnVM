6fb66b971949da3d9b01f11ae61f8b6593a3f4b9
minjun <hoana11@naver.com>
2021-06-25 17:01:53 +0900

    1st

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like Test minjun!');
+    res.send('I like Test jim11!');
 });
 
 app.get('/start', function(req, res){
