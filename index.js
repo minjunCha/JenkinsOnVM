83fcd7d150f0882fc111f185a7fb287e445886b4
minjun <hoana11@naver.com>
2021-06-25 13:53:34 +0900

    7rd

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like Test BlackPink!');
+    res.send('I like Test BTS!');
 });
 
 app.get('/start', function(req, res){
